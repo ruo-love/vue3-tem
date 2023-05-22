c3cb4253ac1a3bd56d3ffd3da4a03a4ec6be9578
qiancheng.zhao <qiancheng.zhao@alo7.com>
2023-05-22 15:20:57 +0800

    feature

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
 export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    'autoprefixer': {
+      overrideBrowserslist: [
+        "Android 4.1",
+        "iOS 7.1",
+        "Chrome > 31",
+        "ff > 31",
+        "ie >= 8",
+        'last 10 versions', // 所有主流浏览器最近2个版本
+      ],
+    },
+    // 'postcss-pxtorem': {
+    //   rootValue: 75, // 75表示750设计稿，37.5表示375设计稿
+    //   propList: ['*']//（数组）可以从 px 更改为 rem 的属性  使用通配符*启用所有属性。例子：['*']
+    // }
   },
 }
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <router-view />
-  <h1 className="text-3xl font-bold underline text-cyan-700">Hello world!</h1>
+  <h1 class="a text-3xl font-bold underline text-cyan-700 flex justify-between">Hello world!</h1>
 </template>
-
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.a {
+  height: 200px;
+}
+</style>
